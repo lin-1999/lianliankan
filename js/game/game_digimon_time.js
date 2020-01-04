1d87504286dd21c90a1f2bdad7cd120507e48585
lin-1999 <57160381+lin-1999@users.noreply.github.com>
2020-01-04 19:46:14 +0800

    Update game_digimon_time.js

--- js/game/game_digimon_time.js
+++ js/game/game_digimon_time.js
@@ -274,6 +274,7 @@ window.onload = function() {
     document.getElementById("home").addEventListener("click", backHome, false);
     document.getElementById("start").addEventListener("click", playButtonSound, false);
     document.getElementById("reset").addEventListener("click", playButtonSound, false);
+    document.getElementById("oneMore").addEventListener("click", oneMoreGame,false);
     back_music = document.getElementById("digimon_music");
     buttonSound = document.getElementById("button_sound");
     isPlaying = false;
@@ -304,6 +305,12 @@ function backHome() {
     buttonSound.play();
     
     setTimeout(function(){ location.href = 'index.html'; }, 200);
+}
+
+function oneMoreGame() {
+    buttonSound.play();
+    
+    setTimeout(function(){ location.href = 'game_digimon_time.html'; }, 200);
 }
 
 function playButtonSound() { buttonSound.play(); }
